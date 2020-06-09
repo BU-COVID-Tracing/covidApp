005f8cbba78cb82d9097d445e3bb8e97b8550fe1
sean333 <seanbrandenburg333@gmail.com>
2020-06-09 14:45:00 -0400

    starting database integration

--- backend/src/main/java/bu/COVIDApp/restservice/ContactCheck/Accessors/KeyValBloomFilterAccessor.java
+++ backend/src/main/java/bu/COVIDApp/restservice/ContactCheck/Accessors/KeyValBloomFilterAccessor.java
@@ -15,17 +15,22 @@ public class KeyValBloomFilterAccessor extends RegistryAccessor {
     }
 
     /**
-     *
-     * @return
+     * Get a bloom filter from the registry so that users can locally check the keys they have collected against it
+     * @return A registry get response containing a bloom filter
      */
     @Override
     public RegistryGetResponse getKeys() {
         return null;
     }
 
+    /**
+     * A user provides a list of keys that they have enoucntered and a check is done against the bloom filter here
+     * with a positive or negative result returned back to the user
+     * @param myKeys A list of keys to check against your local version of the registry
+     * @return true if a matching key was found, false otherwise
+     */
     @Override
     public Boolean postKeys(ArrayList<Object> myKeys) {
-
         return null;
     }
 }
